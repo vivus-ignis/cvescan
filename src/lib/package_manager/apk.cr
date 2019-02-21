5fc4c351d31ca003f522696723d5bd93bf56a63a
Vivus Ignis <ignis@posteo.de>
2019-02-21 08:49:30 +0100

    collecting installed packages on alpine

--- src/lib/package_manager/apk.cr
+++ src/lib/package_manager/apk.cr
@@ -1,6 +1,8 @@
 class PackageManager::Apk < PackageManager
   def initialize
     super
+
+    Utils.dputs "installed : #{Utils.snip @installed} (count: #{@installed.size})"
   end
 
   private def architecture : String
@@ -13,7 +15,12 @@ class PackageManager::Apk < PackageManager
 
   # /lib/apk/db/installed
   def installed
-    [] of Package
+    File.read_lines("/lib/apk/db/installed")
+      .select { |line| line.starts_with?("P:") || line.starts_with?("V:") }
+      .in_groups_of(2)
+      .map { |pkg_a| pkg_a.map { |x| x.not_nil!.split(":") }.to_h }
+      .map { |pkg_h| Package.new(pkg_h["P"],
+                                 pkg_h["V"]) }
   end
 
   def available
